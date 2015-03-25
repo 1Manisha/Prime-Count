42d702a9f3b9e0d8254674b82fbcaa456d06767e
1Manisha <mplightmight@gmail.com>
2015-03-25 12:34:44 +0000

    Prime Exercise
    Basic files

---
+++ Prime Count/MultipleViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MultipleViewController.swift
+//  Prime Count
+//
+//  Created by Manisha Patel on 25/03/2015.
+//  Copyright (c) 2015 Manisha Patel. All rights reserved.
+//
+
+import Foundation
+import  UIKit
+
+class MultipleViewController : UIViewController {
+    
+    @IBOutlet weak var MultipleTableView: UITableView!
+    
+    var multiple = ""
+    
+    override func viewDidLoad() {
+  
+    
+    }
+    
+}
